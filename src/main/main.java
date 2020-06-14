7ef5e77d3519c187ab496f243c9eae4c5ba3ca03
dehoyos9804 <dehoyos9804@gmail.com>
2020-06-13 19:14:03 -0500

    resolviendo errores

--- src/main/main.java
+++ src/main/main.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Main;
+package main;
 
-import Graficas.Graficas;
+import graficas.Graficas;
 import java.awt.Color;
 import java.awt.Window;
 import javax.swing.SwingUtilities;
